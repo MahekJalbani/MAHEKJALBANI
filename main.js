2903a331992b7d7a21ad4b8be77413fbb3f47bb1
Mahek Jalbani <mahek7660@gmail.com>
2023-09-27 15:05:31 +0500

    task 1

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+function calculatePercentage(number, totalAmount) {
+    if (typeof number !== 'number' || typeof totalAmount !== 'number' || totalAmount === 0) {
+      return "Invalid input. Both parameters should be numbers, and totalAmount should not be zero.";
+    }
+  
+    const percentage = (number / totalAmount) * 100;
+    return percentage;
+  }
+  
+  // Example usage:
+  const number = 25;
+  const totalAmount = 100;
+  const result = calculatePercentage(number, totalAmount);
+  console.log(`The percentage is: ${result}%`);
+  
